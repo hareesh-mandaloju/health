537db563dfc3497816ac1b838d81af23a01ed6a6
hareesh <hareesh.gurukul@gmail.com>
2016-06-03 00:04:47 +0530

    updated app

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,12 @@
 import webapp2
 import os
-import google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import template
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path))
+        self.response.out.write(template.render(path, {}))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
